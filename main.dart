49417fbe0ab6654d096ab46996a1fb7531397d31
mirfahim <54933139+mirfahim@users.noreply.github.com>
2019-11-28 14:24:55 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './ui/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Weight on planet",
+    home: new Home(),
+        )
+      
+      
+      );
+      
+    }
+    
+    
