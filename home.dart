6a8ca01006e5af69917d9bd88102075e32a8ea80
mirfahim <54933139+mirfahim@users.noreply.github.com>
2019-11-28 14:25:18 +0600

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    
+    return new HomeState();
+      }
+    
+    }
+    
+    class HomeState extends State<Home> {
+      final TextEditingController _weightController = new TextEditingController();
+      int radioValue = 0;
+      
+      double _finalResult =0.0;
+      void handleRadioValueChanged(int value){
+        setState(() {
+          radioValue = value;
+          switch (radioValue) {
+            case 0 :
+            _finalResult = calculateWeight(_weightController.text, 0.06);
+                          
+                          break;
+                         case 1:
+                          _finalResult = calculateWeight(_weightController.text, 0.38);
+                         
+                          break;
+                          case 2:
+                           _finalResult = calculateWeight(_weightController.text, 0.91);
+            
+                      }
+                    });
+                  }
+              @override
+              Widget build(BuildContext context) {
+                
+                return new Scaffold(
+                  appBar: AppBar(title: new Text('Ojonmapa machine'),
+                  
+                 centerTitle: true,
+                 backgroundColor: Colors.green, 
+                  ),
+                  backgroundColor: Colors.white,
+            
+                  body: new Container(
+                    alignment: Alignment.topCenter,
+                    child: new ListView(
+                      padding: const EdgeInsets.all(2.5),
+                      children: <Widget>[
+                        new Image.asset('assets/planet.png',height:133.0, width:200.0),
+                        
+                        new Container(
+                          margin:const EdgeInsets.all(5.0),
+                          alignment: Alignment.center,
+                          child: new Column(
+                            children: <Widget>[
+            
+                              new TextField(
+                                controller: _weightController,
+                                keyboardType: TextInputType.number,
+                                decoration:new InputDecoration(
+                                    labelText: 'Your Waight on Earth',
+                                    hintText: 'in pound',
+                                    icon: new Icon(Icons.person_outline),
+            
+                                ),
+                              ),
+            
+                              new Padding(padding: new EdgeInsets.all(5.0),),
+                              new Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  new Radio<int>(
+                                    activeColor: Colors.green,
+                                       value:0, groupValue: radioValue, onChanged: handleRadioValueChanged,
+                                       
+                                  ),
+                                  new Text('Pluto', style: new TextStyle(color:Colors.green)),
+                                  new Radio<int>(
+                                    activeColor: Colors.green,
+                                       value:1, groupValue:radioValue, onChanged: handleRadioValueChanged,
+                                  ),
+                                  new Text('Mars', style: new TextStyle(color:Colors.green)),
+                                  new Radio<int>(
+                                    activeColor: Colors.green,
+                                       value:2, groupValue: radioValue, onChanged: handleRadioValueChanged,
+                                  ),
+                                  new Text('Venus', style: new TextStyle(color:Colors.green)),
+            
+                                ],
+                                ),
+                                new Padding(padding: new EdgeInsets.all(12.0),),
+                    new Text(
+                      "$_finalResult",
+                     style: new TextStyle(
+                         color:Colors.green,
+                         fontSize:20,
+                         fontWeight: FontWeight.w500,
+            
+            
+                      ),
+            
+                    ),
+                            ],
+            
+                          ),
+            
+                        ),
+            
+            
+                      ],
+            
+                    ),
+                  ),
+            
+                  
+            
+            
+                );
+               
+              }
+            
+             double calculateWeight(String weight, double multiplier) {
+               if(int.parse(weight).toString().isNotEmpty && int.parse(weight)>0)
+              {return int.parse(weight)* multiplier;
+              } else{
+                print("wrong");
+                return int.parse("180")* 0.38 ;
+              }
+             }
+}
